d78712cbfad897da830081d08184f7aac6fa9c01
Yoonho Cho <josephcyh@yonsei.ac.kr>
2026-03-12 16:21:15 +0900

    실제 경로로 데이터 베이스 연결 수정

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ax_test/', // GitHub 저장소 이름에 맞게 수정하세요
+  base: '/test-page/', // GitHub 저장소 이름에 맞게 수정하세요
 })
